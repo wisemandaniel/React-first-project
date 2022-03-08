fe0a3440b6e10a7079c1a834629118261fe2b9cb
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 20:38:45 +0100

    learned how to create a component and nest it inside another

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const like = 50;
-  const Array = [1,2,3,4,5];
-  const isTrue = false;
-  const Obj = {name: 'Dan', age: 22}
   
   return (
     <div className="App">
-      <header className="App-header">
-          <div className="content">
-              <h1>{title}</h1>  
-              <p>Liked {like} times</p>
-              <p>{Array[1]}</p>
-              <p>{Obj.age}</p>
-              <p>{'Hello' + ' ' +  Obj.name}</p>
-              <p>{ Math.random() * 10}</p>
-          </div>
-      </header>
+      <Navbar />
+      <div className="content">
+          <Home />
+      </div>
     </div>
   );
 }
