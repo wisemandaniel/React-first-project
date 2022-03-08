421d9e8b39bf833675400acd94aaeca9c67db151
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 20:17:43 +0100

    Learned how to dynamically render elememnts in a component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const like = 50;
+  const Array = [1,2,3,4,5];
+  const isTrue = false;
+  const Obj = {name: 'Dan', age: 22}
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="content">
+              <h1>{title}</h1>  
+              <p>Liked {like} times</p>
+              <p>{Array[1]}</p>
+              <p>{Obj.age}</p>
+              <p>{'Hello' + ' ' +  Obj.name}</p>
+              <p>{ Math.random() * 10}</p>
+          </div>
       </header>
     </div>
   );
