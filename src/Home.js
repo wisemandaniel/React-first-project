a120c2605673f6bd4c51fc2ef34ebfc691e20487
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 21:50:56 +0100

    learned how to add the react developer tool to my browser and how to use it

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState('Damiel');
+    const [name, setName] = useState({name: 'NDZO DANIEL', age: 22, height: '1.78m', IQ: 152});
     const handleClick = () => {
-        setName('NDZO');
+        setName({name: 'AKO BRONHILDA', age: 22, height: '1.78m', IQ: 152});
     } 
     return ( 
         <div className="home">
            <h2>Home component</h2> 
-           <p>{name}</p>
+           <p>{name.name}</p>
            <button onClick={handleClick}>Click me</button>
         </div>
      );
