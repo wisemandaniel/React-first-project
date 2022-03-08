95247e4794bde6e6aa0ba0d39e5723826fc5daaa
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 22:14:29 +0100

    learned how to use output list in react using the map function

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState({name: 'NDZO DANIEL', age: 22, height: '1.78m', IQ: 152});
-    const handleClick = () => {
-        setName({name: 'AKO BRONHILDA', age: 22, height: '1.78m', IQ: 152});
-    } 
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+
+    // const handleAddBlog = () => {
+    //     setBlogs({
+            
+    //     })
+    // }
     return ( 
         <div className="home">
-           <h2>Home component</h2> 
-           <p>{name.name}</p>
-           <button onClick={handleClick}>Click me</button>
+                { blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </div>
+                ))}  
         </div>
      );
 }
