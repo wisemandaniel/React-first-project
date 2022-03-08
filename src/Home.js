f28f3f7764931c4869726d72f9d2cb93377505ce
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-09 00:14:13 +0100

    learned how to a fetch data from a json-server and update the useState in the useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     
     const handleDelete = (id) => {
@@ -15,12 +11,18 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('This is useEffecct hook in action');
+        fetch('http://localhost:8000/blogs')
+             .then(res => {
+                 return res.json();
+             })
+             .then(data => {
+                 setBlogs(data)
+             })
     }, [blogs])
 
     return ( 
         <div className="home">
-              <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+              {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
         </div>
      );
 }
