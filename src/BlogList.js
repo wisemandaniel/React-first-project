08c09b83389fe331f01f68b9f54359cf06a360d7
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 23:22:19 +0100

    learned how to use props to pass functions to other components

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
+    const handleDelete = props.handleDelete;
 
     return ( 
         <div className="blog-list">
@@ -9,6 +10,7 @@ const BlogList = (props) => {
                     <div className="blog-preview" key={blog.id}>
                         <h2>{ blog.title }</h2>
                         <p>Written by { blog.author }</p>
+                        <button onClick={() => handleDelete(blog.id)}>delete</button>
                     </div>
             ))}  
         </div>
