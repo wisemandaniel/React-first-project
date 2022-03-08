bcb6a6d495ce01aa396e9a87c05b46f8293aba54
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-09 00:32:17 +0100

    learned how to use the useState together with the conditional statement to show loading when data is being fetched and hide it when the data has been fetched

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,6 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
 
     return ( 
         <div className="blog-list">
@@ -10,7 +9,6 @@ const BlogList = (props) => {
                     <div className="blog-preview" key={blog.id}>
                         <h2>{ blog.title }</h2>
                         <p>Written by { blog.author }</p>
-                        <button onClick={() => handleDelete(blog.id)}>delete</button>
                     </div>
             ))}  
         </div>
--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,26 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs)
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
+       setTimeout(() => {
         fetch('http://localhost:8000/blogs')
-             .then(res => {
-                 return res.json();
-             })
-             .then(data => {
-                 setBlogs(data)
-             })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+        })
+       }, 2000)
     }, [blogs])
 
     return ( 
         <div className="home">
-              {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+              {isPending && <h2>loading...</h2>}
+              {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
