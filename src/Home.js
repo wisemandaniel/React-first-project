6cef2f71fa9b7f915776c86e9f63b17ea11bdea3
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 21:10:29 +0100

    learned how to pass parameter to a function when called

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,17 @@ const Home = () => {
     const handleClick = () => {
        console.log("Hello, ninjas");
     }
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    }
+
     return ( 
         <div className="home">
            <h2>Home component</h2> 
-           <button onClick={handleClick}>Click me</button>   
+           <button onClick={handleClick('DANIEL')}>Click me</button>   
+           <br /><br />
+           <button onClick={() => handleClickAgain('DANIEL')}>Click me</button> 
         </div>
      );
 }
