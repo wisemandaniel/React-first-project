3da720d2ac6efdcbe0fc893ceb733fa0234450bb
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-09 00:52:59 +0100

    How to catch error from the backend

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
        setTimeout(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok) {
+                throw Error('Could not fetch the data for that resource')
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
             setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
         })
        }, 2000)
     }, [blogs])
 
     return ( 
         <div className="home">
-              {isPending && <h2>loading...</h2>}
+              {error && <p>{error}</p>}
+              {isPending && <div>loading...</div>}
               {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
