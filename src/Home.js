614b34eb35eee773c6b1c5ae32e350a2ea7d2c88
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-08 21:01:21 +0100

    learned how to create a function and link it to a button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () => {
+       console.log("Hello, ninjas");
+    }
     return ( 
         <div className="home">
-           <h2>Home component</h2>    
+           <h2>Home component</h2> 
+           <button onClick={handleClick}>Click me</button>   
         </div>
      );
 }
