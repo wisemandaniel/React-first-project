547db2a817fd1ba178dc6a620074088d6dc21321
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-09 01:53:21 +0100

    How to use the cleanup function and the abortcontroller to abort fthe fetch of data when a user navigate to a different component when the fetch is still in progress

--- src/Home.js
+++ src/Home.js
@@ -7,24 +7,32 @@ const Home = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-       setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(!res.ok) {
-                throw Error('Could not fetch the data for that resource')
-            }
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-       }, 2000)
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs', { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if(err.name === 'AbortError') {
+                    console.log('Fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+                 
+            });
+        }, 3000)
+        return () => abortCont.abort(); 
     }, [blogs])
 
     return ( 
