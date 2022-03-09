145de09cf632fb9750f147a1bd13ec898078c441
wisemandaniel <ndzodaniel31@gmail.com>
2022-03-09 01:26:47 +0100

    learned how to use switch, route and exact attribute to navigate from one route to another

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
            <h1>The Dojo blog</h1>
            <div className="links">
                <a href="/">Home</a>
-               <a href="/create">Create</a> 
+               <a href="/create">New Blog</a> 
            </div>    
         </nav>
      );
